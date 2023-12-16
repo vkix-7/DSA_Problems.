07e7d3702fee9d5543916d007229eb6cb5486874
vkix-7 <vijayku123456789@gmail.com>
2023-12-17 00:14:51 +0530

    added new program

---
+++ Valid_Anagram.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.util.Arrays; 
+import java.util.Collections; 
+public class Valid_Anagram {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int map[]=new int[26];
+        for(char x : s.toCharArray()){
+            map[x-'a']++;
+        }
+        for(char x : t.toCharArray()){
+            map[x-'a']--;
+        }
+        for(int i:map){
+            if (i!=0){
+                return false;
+            }
+        }
+        return true;
+        }
+
+public static void main(String args[])  
+{ 
+  String s = "anagram";
+   String t = "nagaram";
+    if (isAnagram (s , t)) 
+            System.out.println("The two strings are"
+                               + " anagram of each other"); 
+        else
+            System.out.println("The two strings are not"
+                               + " anagram of each other"); 
+    } 
+  
+}
